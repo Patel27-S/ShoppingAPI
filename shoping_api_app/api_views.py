3fc0997edb658962869ff6da0bcb0dc52ee4f525
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2022-05-24 20:48:01 -0400

    Added pagination feature to ProductList View, so that not all products would be displayed, and client would be able to in fact bring up as many instances as wanted but not more than max_limit.

--- shoping_api_app/api_views.py
+++ shoping_api_app/api_views.py
@@ -1,9 +1,15 @@
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 from .serializers import ProductSerializer
 from .models import Product
+
+
+class ProductsPagination(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
 
 class ProductList(ListAPIView):
@@ -22,6 +28,7 @@ class ProductList(ListAPIView):
     # Below will enable search on the basis of name and
     # description for client.
     search_fields = ('name', 'description')
+    pagination_class = ProductsPagination
 
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sale', None)
