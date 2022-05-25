7ff8780b9a39be675d71f40551c600a52fa66f31
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2022-05-24 20:00:09 -0400

    Added the functionality where the client can filter product lists on the basis of name and description's searching.

--- shoping_api_app/api_views.py
+++ shoping_api_app/api_views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 from .serializers import ProductSerializer
 from .models import Product
@@ -16,5 +17,26 @@ class ProductList(ListAPIView):
     # use ListAPIView. But, the only thing is that
     # we use a filter and search by 'id'. Hence, need to
     # write the code for so to happen.
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
+    # Below will enable search on the basis of name and
+    # description for client.
+    search_fields = ('name', 'description')
+
+    def get_queryset(self):
+        on_sale = self.request.query_params.get('on_sale', None)
+        # If on_sale is Not defined in the URL, i.e. in the case
+        # when client does not want results on the basis of
+        # on_sale or not.
+        if on_sale is None:
+            return super().get_queryset()
+        else:
+            queryset = Product.objects.all()
+            if on_sale.lower() == 'true':
+                from django.utils import timezone
+                now = timezone.now()
+                return queryset.filter(
+                    sale_start__lte=now,
+                    sale_end__gte=now,
+                )
+            return queryset
